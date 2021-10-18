8e4b8f29c121d148d371d78781c57b7a16bfe8b9
NtwaliHeritier <heritierntwali@gmail.com>
2021-10-18 22:52:30 +0200

    add search functionalities

--- index.js
+++ index.js
@@ -1,40 +1,56 @@
 let countries = []
+let base_url = "https://restcountries.com/v3.1/"
+let url = ""
+let param = "all"
+document.querySelector("#regions").addEventListener("change", (e) => {
+    param = `region/${e.target.value}`
+    const container = document.querySelector(".container")
+    container.innerHTML = ""
+    display_countries()
+})
+
+document.querySelector(".search-country").addEventListener("keyup", (e) => {
+    param = `name/${e.target.value}`
+    const container = document.querySelector(".container")
+    container.innerHTML = ""
+    display_countries()
+})
+
 const display_countries = async() => {
-    const data = await fetch("https://restcountries.com/v3.1/all")
+    url = base_url + param
+    const data = await fetch(url)
     const response =  await data.json()
     const container = document.querySelector(".container")
-    for(let country of response) {
-        console.log(country)
-
-        const country_div = document.createElement("div")
-        add_country_div_class(country_div)
-
-        const img = document.createElement("img")    
-        img.setAttribute("src", country.flags.png)
-        add_img_class(img)
-
-        const country_name = document.createElement("h4")
-        country_name.innerText = country.name.common
-        add_country_name_class(country_name)
-
-        const population = document.createElement("h5")
-        population.innerHTML = `<b>Population:</b> ${country.population}`
-
-        const region = document.createElement("h5")
-        region.innerHTML = `<b>Region:</b> ${country.region}`
-
-        const capital = document.createElement("h5")
-        region.innerHTML = `<b>Capital:</b> ${country.capital[0]}`
-
-        const specs = document.createElement("div")
-        specs.classList.add("ml-2")
-
-        specs.appendChild(country_name)
-        specs.appendChild(population)
-        specs.appendChild(region)
-        country_div.appendChild(img)
-        country_div.appendChild(specs)
-        container.appendChild(country_div)
+    if(response) {
+        for(let country of response) {
+            console.log(country)
+            const country_div = document.createElement("div")
+            add_country_div_class(country_div)
+    
+            const img = document.createElement("img")    
+            img.setAttribute("src", country.flags.png)
+            add_img_class(img)
+    
+            const country_name = document.createElement("h4")
+            country_name.innerText = country.name.common
+            add_country_name_class(country_name)
+    
+            const population = document.createElement("h5")
+            population.innerHTML = `<b>Population:</b> ${country.population}`
+    
+            const region = document.createElement("h5")
+            region.innerHTML = `<b>Region:</b> ${country.region}`
+    
+            const specs = document.createElement("div")
+            specs.classList.add("ml-2")
+    
+            specs.appendChild(country_name)
+            specs.appendChild(population)
+            specs.appendChild(region)
+            country_div.appendChild(img)
+            country_div.appendChild(specs)
+            container.appendChild(country_div)
+        }
     }
 }
 
